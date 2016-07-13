6dd9d04dd4184bf6f23d6de41e7aaee94b05f2fe
bmcorser <benmarshallcorser@gmail.com>
2016-07-13 12:20:17 +0100

    Generalise somewhat

--- similarity/main.py
+++ similarity/main.py
@@ -1,41 +1,27 @@
+import collections
 import os
+import itertools
 
 from . import cosine
 from cache import CallCache, _unpickle, _pickle
 
-cos = _unpickle('ukl.pickle')
-cos.update(_unpickle('bduk.pickle'))
-cos = {co_name: co_desc for co_name, (co_desc, co_url) in cos.items()}
-for name, fc_data in _unpickle('fc.pickle').items():
-    bios = list(filter(
-        None,
-        [profile.get('bio') for profile in fc_data.get('socialProfiles', [])]
-    ))
-    cos[name] = ' '.join(bios + [cos[name]])
-
-opps = _unpickle('eig.pickle')
-opps.extend(_unpickle('tenderpage-gb.pickle'))
-opps.extend(_unpickle('tenderpage-us.pickle'))
-
 cache = CallCache(path=os.path.dirname(__file__), func=cosine.main)
 
-def dress_up(scores, n=10):
-    best = [
-        (score, opps[scores.index(score)])
-        for score in sorted(scores, reverse=True)[:n]
-    ]
+def dress_up(scores, context, n=10):
+    best = []
+    for score in sorted(scores, reverse=True)[:n]:
+        index = scores.index(score)
+        context_item = next(itertools.islice(context.items(), index, index + 1))
+        best.append((score, context_item))
 
     return [
         {
             'score': score,
-            'title': opp_title,
-            'desc': opp_desc,
-            'enddate': opp_enddate,
+            'name': name,
+            'desc': desc,
         }
-        for score, (opp_title, opp_desc, opp_enddate) in best
+        for score, (name, desc) in best
     ]
 
-def main(name):
-    if name == 'l':
-        return list(map(print, sorted(cos.keys())))
-    return(dress_up(cache.fetch(cos[name], [desc for _, desc, _ in opps])))
+def main(subject, context):
+    return dress_up(cache.fetch(subject, list(context.values())), context)
