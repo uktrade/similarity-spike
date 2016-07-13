1a04a6da8213efe5679302a8badc37cf20079a81
bmcorser <benmarshallcorser@gmail.com>
2016-07-13 14:28:07 +0100

    Indexing both ways

--- similarity/index.py
+++ similarity/index.py
@@ -1,22 +1,26 @@
 import itertools
 import multiprocessing as mp
-from .main import cache, cos, opps
+from cache import _unpickle
+from .main import cache
 
+cos_odict = _unpickle('cos_odict.pickle')
+opps_odict = _unpickle('opps_odict.pickle')
 
-def func(co_name, co_desc, opp_descs):
-    print(co_name)
+def func(subject, context):
+    print(subject[:20])
     try:
-        cache.fetch(co_desc, opp_descs)
-    except:
-        print("{0} failed".format(co_name))
+        cache.fetch(subject, context)
+    except Exception as exc:
+        print("FAIL {0}".format(subject[:20]))
 
 
 def main():
-    opp_descs = [desc for _, desc, _ in opps]
     pool = mp.Pool(8)
     # starmap = itertools.starmap
     starmap = pool.starmap
-    list(starmap(func, [
-        (co_name, co_desc, opp_descs)
-        for co_name, co_desc in cos.items()
-    ]))
+
+    co_descs = list(cos_odict.values())
+    list(starmap(func, [(opp_desc, co_descs) for opp_desc in opps_odict.values()][:3]))
+
+    opp_descs = list(opps_odict.values())
+    list(starmap(func, [(co_desc, opp_descs) for co_desc in cos_odict.values()]))
